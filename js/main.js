22ce7d55a5c68b547a8c0a5261a1994abdc9a0a0
reifred <reifred33@gmail.com>
2019-01-16 10:24:11 +0300

    Implement feedback

---
+++ js/main.js
@@ -0,0 +1,35 @@
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i=0; i<coll.length; i++){
+      coll[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var records_content = this.nextElementSibling;
+        if(records_content.style.display === "block"){
+          records_content.style.display = "none";
+          document.getElementById("user_name_top").style.display = "block";
+
+        }else{
+          records_content.style.display = "block";
+          document.getElementById("user_name_top").style.display = "none";
+        }
+      });
+    }
+
+  function getLocation(){
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+      }else{
+        alert("Failed to get location insert it manually")
+      }
+  }
+  function showPosition(position){
+    document.getElementById("lat").value = position.coords.latitude;
+    document.getElementById("long").value = position.coords.longitude;    
+  }
+  
+    function openForm(){
+      document.getElementById("myForm").style.display = "block";
+    }
+    function closeForm(){
+      document.getElementById("myForm").style.display = "none";
+    }
